72f465618f316bec066ec675b7794530f49463af
Joshua Haertel <josh.haertel@gmail.com>
2020-04-21 21:43:21 -0600

    Finish rename

--- indexroyal/wsgi.py
+++ indexroyal/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for indexroyale project.
+WSGI config for indexroyal project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indexroyale.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indexroyal.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indexroyale.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indexroyal.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
